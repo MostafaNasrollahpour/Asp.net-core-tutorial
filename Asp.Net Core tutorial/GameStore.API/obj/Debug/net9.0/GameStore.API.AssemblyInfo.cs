6bcf0c549035bf97b405bc48a8eb125fe3d6dcfd
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2025-02-21 18:41:16 +0330

    Defining the data model completed

--- Asp.Net Core tutorial/GameStore.API/obj/Debug/net9.0/GameStore.API.AssemblyInfo.cs
+++ Asp.Net Core tutorial/GameStore.API/obj/Debug/net9.0/GameStore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86da52cb84799126c785d9ef89b2361945bd954e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f6ef20ab53e2361cedacd0dcc5952c53ff51089")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
